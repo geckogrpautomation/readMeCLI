8c1c54a9791347a4dacde923e9d7d1d3aa54f967
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2021-01-20 16:43:11 +1100

    Markup split via comma done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // TODO: Include packages needed for this application
-let fs = require("fs");
-let inquirer = require("inquirer");
-let obj = require('./obj');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const obj = require('./obj');
 // TODO: Create an array of questions for user input
-let licList = ["PDL", "GNU/LGPL" , "PrivatPermissive","Copyleft", "Proprietary"];
+const licList = ["PDL", "GNU/LGPL" , "PrivatPermissive","Copyleft", "Proprietary"];
 
 
 //Create objects from constructors which are defined in obj.js
@@ -12,8 +12,8 @@ let readmeTitle = new obj.InquirerInpNumCnfrmPass("readmetitle", "input", "What 
 let description = new obj.InquirerInpNumCnfrmPass("description", "input", "Please enter a description about your project.");
 let contributors = new obj.InquirerInpNumCnfrmPass("contributor", "input", "Please enter the contributors to the project and if more than one separate via a comma.");
 let usage = new obj.InquirerInpNumCnfrmPass("usage", "input", "Please add a link to a video of how / when to use the code.");
-let preReq = new obj.InquirerInpNumCnfrmPass("prereq", "input", "What are the required prerequisites or project dependancies?");
-let installation = new obj.InquirerInpNumCnfrmPass("installation", "input", "Please define the installation steps separated by a comma Eg 1 - npm init, 2 - npm install inquirer?");
+let preReq = new obj.InquirerInpNumCnfrmPass("prereq", "input", "What are the required prerequisites or project dependancies and if more than one separate via a comma.?");
+let installation = new obj.InquirerInpNumCnfrmPass("installation", "input", "Please define the NPM installation steps separated by a comma Eg 1 - npm init, 2 - npm install inquirer?");
 let builtWith = new obj.InquirerInpNumCnfrmPass("builtwith", "input", "Please define the technologies used in this project separated by a comma Eg 1 - HTML, 2 - JS?");
 let license = new obj.InquirerListChkBx("license", "list", "Select correct license type of this project.",licList, "Proprietary" );
 let gitHubLink = new obj.InquirerInpNumCnfrmPass("githublink", "input", "Please provide the link to your project's GitHub repository?");
@@ -51,47 +51,81 @@ inquirer
   });
 
 
-
-// TODO: Create a function to write README 
+//Build read me file via template and user input
 function writeToFile(answers) {
 
 //Define read me filename with file extension  
 let filename = `${answers.readmetitle}.md`
+
+//Split user strings via , delimiter
+let contributorArr = answers.contributor.split(",");
+
+let preReqArr = answers.prereq.split(",");
+
+let installationArr = answers.installation.split(",");
+
+let builtWithArr = answers.builtwith.split(",");
+
+
+//Define markup strings
+let contributorMarkup = "";
+let preReqMarkup = "";
+let installationMarkup = "";
+let builtWithMarkup = "";
+let str = "";
+
+//Build required markup for insertion into template for each of the required string arrays derived from the comma separated user input.
+contributorArr.forEach(data => {
+  str = "";
+  str = (`* ${data} \n`);
+  contributorMarkup = contributorMarkup.concat(str);  
+});
+console.log(contributorMarkup);
+
+preReqArr.forEach(data => {
+  str = "";
+  str = (`* ${data} \n`);
+  preReqMarkup = preReqMarkup.concat(str);  
+});
+console.log(preReqMarkup);
+
+installationArr.forEach(data => {
+  str = "";
+  str = (`* ${data} \n`);
+  installationMarkup = installationMarkup.concat(str);  
+});
+console.log(installationMarkup);
+
+builtWithArr.forEach(data => {
+  str = "";
+  str = (`* ${data} \n`);
+  builtWithMarkup = builtWithMarkup.concat(str);  
+});
+console.log(builtWithMarkup);
+
 //Read me file template
 let readmeFormatted = 
 `
+
 #${answers.projtitle}
-## `filename`
-
-[![Contributors][contributors-shield]][contributors-url]
-[![Forks][forks-shield]][forks-url]
-[![Stargazers][stars-shield]][stars-url]
-[![Issues][issues-shield]][issues-url]
-[![MIT License][license-shield]][license-url]
-[![LinkedIn][linkedin-shield]][linkedin-url]
-
-
-<br />
+<br/>
+##${filename}
+<br/>
 <p align="center">
   <a href="${answers.githublink}">${answers.githubname}
     <img src="images/logo.png" alt="Logo" width="80" height="80">
   </a>
-
-  <h3 align="center">${answers.projtitle}</h3>
-
-  <p align="center">
+  <h2 align="center">${answers.projtitle}</h3> 
   <h4 align="center">Project Description</h3>
-    ${answers.description}
-    <br />
+  <p align="center"${answers.description}>    
+    <br/>
     <a href="${answers.githublink}<strong>Link to the repo. »</strong></a>
-    <br />
-    <br />
+    <br/>
+    <br/>
     <a href="${answers.githublink}/issues">Bug Report</a>
-    ·
     <a href="${answers.githublink}/issues">Feature Request</a>
   </p>
 </p>
-
 
 <details open="open">
   <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
@@ -103,24 +137,20 @@ let readmeFormatted =
       </ul>
     </li>
     <li>
-      <a href="#getting-started">Getting Started</a>
+      <a href="getting-started">Getting Started</a>
       <ul>
-        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#prerequisite">Prerequisites</a></li>
         <li><a href="#installation">Installation</a></li>
+        <li><a href="#usage-video">Installation</a></li>        
       </ul>
     </li>
-    <li><a href="#builtWith">Usage</a></li>
-    <li><a href="#roadmap">Roadmap</a></li>
     <li><a href="#contributing">Contributing</a></li>
     <li><a href="#license">License</a></li>
     <li><a href="#contact">Contact</a></li>
-    <li><a href="#acknowledgements">Acknowledgements</a></li>
   </ol>
 </details>
 
 
-
-<!-- ABOUT THE PROJECT -->
 ## About The Project
 
 ${answers.description}
@@ -130,16 +160,16 @@ ${answers.description}
 
 ### Built With
 
-${answers.builtwith}
+${builtWithMarkup}
 
 
 ## Getting Started
 
 To get a local copy up and running follow these simple steps.
 
-### Prerequisites
+### Prerequisite
 
-*${answers.prereq}
+${preReqMarkup}
   
 
 ### Installation
@@ -151,9 +181,9 @@ To get a local copy up and running follow these simple steps.
    
 2. Install NPM packages
    
-   npm install
+${installationMarkup}
 
-   The repo should have a package.JSON and a package-lock.JSON file which lists all the dependancies. Hence only npm install is required.
+The repo should have a package.JSON and a package-lock.JSON file which lists all the dependancies. Hence only npm install is required.
    
 
 ## Usage video
@@ -163,11 +193,11 @@ See below link for a link on how to use our code.
 ${answers.usage}
 
 
-## Contributors
+## Contributing
 
 Thanks to all the contributors on this project. You are legends! 
 
-* ${answers.contributor}
+${contributorMarkup}
 
 In order to contribute. Please follow the below:
 
@@ -182,7 +212,7 @@ In order to contribute. Please follow the below:
 
 Distributed under the folllowing license:
 
-${answers.license}
+* ${answers.license}
 
 
 ## Contact
@@ -203,8 +233,3 @@ Project Link: ${answers.githublink})
   });
 }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-//init();
--- readme.md
+++ readme.md
@@ -1,1 +1,133 @@
 
+#projhect
+<br/>
+##asdfasd.md
+<br/>
+<p align="center">
+  <a href="asdffasdfaasd">asdfasdasd
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h2 align="center">projhect</h3> 
+  <h4 align="center">Project Description</h3>
+  <p align="center">asdfsdfsdafas></p>
+    
+    <br/>
+    <a href="asdffasdfaasd<strong>Link to the repo. »</strong></a>
+    <br/>
+    <br/>
+    <a href="asdffasdfaasd/issues">Bug Report</a>
+    ·
+    <a href="asdffasdfaasd/issues">Feature Request</a>
+  </p>
+</p>
+
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisite">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+        <li><a href="#usage-video">Installation</a></li>        
+      </ul>
+    </li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+## About The Project
+
+asdfsdfsdafas
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+
+### Built With
+
+* html 
+* css 
+* js 
+
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisite
+
+* a 
+* b 
+* c 
+* d 
+
+  
+
+### Installation
+
+1. Clone the repo.
+   Open Git Bash or similar into your required directory.
+
+   git clone adfasdfdfa
+   
+2. Install NPM packages
+   
+  * 4 
+* 5 
+* 6 
+* 7 
+
+
+   The repo should have a package.JSON and a package-lock.JSON file which lists all the dependancies. Hence only npm install is required.
+   
+
+## Usage video
+
+See below link for a link on how to use our code.
+
+fdsfas
+
+
+## Contributing
+
+Thanks to all the contributors on this project. You are legends! 
+
+* 1 
+* 2 
+* 3 
+* 4 
+
+
+In order to contribute. Please follow the below:
+
+1. Fork the Project
+2. Create your Feature Branch (git checkout -b feature/AmazingFeature)
+3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+4. Push to the Branch (git push origin feature/AmazingFeature)
+5. Open a Pull Request
+
+
+## License
+
+Distributed under the folllowing license:
+
+* PrivatPermissive
+
+
+## Contact
+
+asdfasdasd - afsdsdafasd - email
+
+Project Link: asdffasdfaasd)
+
